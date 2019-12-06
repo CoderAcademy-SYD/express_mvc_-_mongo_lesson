50bbed5d6c2082a88c4a37d435e128cb078b5162
Bruce McClure <brucemcc90@gmail.com>
2019-12-06 14:42:53 +1100

    refactored in async await

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,22 +1,25 @@
 const ContactModel = require('../database/models/contact_model') // require in the ContactModel, this is how we will enter data to the db
 
-let index = (req, res) => {
-  ContactModel.find() // Find everything in the ContactsModel
-    .then(contacts => {
-      return res.render('contacts/index', { contacts }) // Sending the data contacts to 'contacts/index' similar to the '@'in ruby
-    })
-    .catch(err => {
-      return res.status(500).send(`Error: ${err}`)
-    })
+let index = async (req, res) => {
+  const contacts = await ContactModel.find() // Find everything in the ContactsModel
+  try {
+    res.render('contacts/index', { contacts }) // Sending the data contacts to 'contacts/index' similar to the '@'in ruby
+  } catch (err) {
+    res.status(500).send(`Error: ${err}`)
+  }
 }
 
 // Where we send all of our data
-let create = (req, res) => {
+let create = async (req, res) => {
   let { name, email, enquiry } = req.body // Destructure name and email off req.body
-  let contact = { name, email, enquiry } // make contact a variale which holds an object of the name and email
-  ContactModel.create(contact) // Create a new contact in the db with the object we passed in. returns a promise
-    .then(() => res.redirect('/contacts')) // Redirect the user to '/contacts' to see the list of cantacts
-    .catch(err => res.status(500).send(`Error`, `${err}`)) // 500 = internal error, send back the error
+  let newContact = { name, email, enquiry } // make contact a variale which holds an object of the name and email
+
+  try {
+    await ContactModel.create(newContact) // Create a new contact in the db with the object we passed in. returns a promise
+    res.redirect('/contacts') // Redirect the user to '/contacts'
+  } catch (err) {
+    res.status(500).send(`Error`, `${err}`) // 500 = internal error, send back the error
+  }
 }
 
 // 'new' is a reserved word in JS. Therefore we chose newResourse so it will be generic and reuseable
